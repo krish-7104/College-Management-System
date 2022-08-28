1df2f60297a7f63ef8abf09008a8d0423140595b
Krish Jotaniya <95702005+krish-7104@users.noreply.github.com>
2022-08-28 08:39:47 +0530

    Update firebase.js

--- src/backend/firebase.js
+++ src/backend/firebase.js
@@ -2,15 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCp_XQxkjeXVIGsvo8SiEqp3gNe2H7bLqE",
-  authDomain: "college-managment-system-krish.firebaseapp.com",
-  projectId: "college-managment-system-krish",
-  storageBucket: "college-managment-system-krish.appspot.com",
-  messagingSenderId: "185930233705",
-  appId: "1:185930233705:web:918e94f5fa437f2fc4aff8",
-  measurementId: "G-WPEKB1E3YZ",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: ",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
-
+//Add Your Own Project Details Here Above
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 export { db };
