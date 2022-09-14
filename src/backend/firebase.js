834c0e72c6c6ce484e75b062c6db96ee97bc4f33
Krish Jotaniya <jotaniyakrish07@gmail.com>
2022-09-14 13:53:21 +0530

    database changed

--- src/backend/firebase.js
+++ src/backend/firebase.js
@@ -3,12 +3,11 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API,
-  authDomain: "rannumber-guess.firebaseapp.com",
-  databaseURL: "https://rannumber-guess-default-rtdb.firebaseio.com",
-  projectId: "rannumber-guess",
-  storageBucket: "rannumber-guess.appspot.com",
-  messagingSenderId: "949978109505",
-  appId: "1:949978109505:web:4a09f6895b2b0458ec98c0",
+  authDomain: "college-managment-system-krish.firebaseapp.com",
+  projectId: "college-managment-system-krish",
+  storageBucket: "college-managment-system-krish.appspot.com",
+  messagingSenderId: "185930233705",
+  appId: "1:185930233705:web:918e94f5fa437f2fc4aff8",
 };
 
 const app = initializeApp(firebaseConfig);
