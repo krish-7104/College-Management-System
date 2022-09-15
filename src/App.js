64d1912cfdb1ede947fa1eea2ede190eeb477219
Krish Jotaniya <jotaniyakrish07@gmail.com>
2022-09-15 17:32:38 +0530

    navigation

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from "react";
 import "./style/App.css";
 import Login from "./components/Login";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import StudentHome from "./components/StudentHome";
 import FacultyHome from "./components/FacultyHome";
 import Error from "./components/Error";
 const App = () => {
   return (
     <React.StrictMode>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/students-home" element={<StudentHome />} />
-        <Route path="/faculty-home" element={<FacultyHome />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/students-home" element={<StudentHome />} />
+          <Route path="/faculty-home" element={<FacultyHome />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </Router>
     </React.StrictMode>
   );
 };
