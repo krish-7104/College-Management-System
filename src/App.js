49a56309f38da37f9e711fdff428c32d373b0608
Krish Jotaniya <jotaniyakrish07@gmail.com>
2022-09-22 19:53:50 +0530

    timetable section added and responsive ness added

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ const App = () => {
       <Router>
         <Routes>
           <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
           <Route path="students-home" element={<StudentHome />} />
           <Route path="faculty-home" element={<FacultyHome />} />
           <Route path="/admin" element={<AdminLogin />} />
