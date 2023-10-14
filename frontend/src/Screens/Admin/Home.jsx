f9db3ceedbfdbded9ae8ceff83d0161536e192e5
Krish Jotaniya <krishjotaniya71@gmail.com>
2023-10-14 21:25:49 +0530

    Admin updated

--- frontend/src/Screens/Admin/Home.jsx
+++ frontend/src/Screens/Admin/Home.jsx
@@ -85,7 +85,7 @@ const Home = () => {
       {load && (
         <>
           <Navbar />
-          <div className="w-[90%] mx-auto mt-8 flex justify-center items-start flex-col container">
+          <div className="w-[100%] mx-auto mt-8 flex justify-center items-start flex-col container">
             <ul className="flex justify-evenly items-center gap-10 w-[90%] mx-auto">
               <li
                 className={`text-center rounded-sm px-4 py-2 w-1/5 cursor-pointer ease-linear duration-300 hover:ease-linear hover:duration-300 hover:transition-all transition-all ${
@@ -97,7 +97,16 @@ const Home = () => {
               >
                 Profile
               </li>
-
+              <li
+                className={`text-center rounded-sm px-4 py-2 w-1/5 cursor-pointer ease-linear duration-300 hover:ease-linear hover:duration-300 hover:transition-all transition-all ${
+                  selectedMenu === "Student"
+                    ? "border-b-2 pb-2 border-blue-500 bg-blue-100 rounded-sm"
+                    : "bg-blue-500 text-white hover:bg-blue-600 border-b-2 border-blue-500"
+                }`}
+                onClick={() => setSelectedMenu("Student")}
+              >
+                Student
+              </li>
               <li
                 className={`text-center rounded-sm px-4 py-2 w-1/5 cursor-pointer ease-linear duration-300 hover:ease-linear hover:duration-300 hover:transition-all transition-all ${
                   selectedMenu === "Faculty"
