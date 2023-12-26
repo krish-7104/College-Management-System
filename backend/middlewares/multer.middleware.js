2f9a4a5b43db5ca09db7e0f693ff8ad6e4615144
Krish Jotaniya <krishjotaniya71@gmail.com>
2023-12-26 21:51:58 +0530

    Backend: Multer Added

---
+++ backend/middlewares/multer.middleware.js
@@ -0,0 +1,15 @@
+const multer = require("multer")
+const uuidv4 = require("uuid").v4
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "./public/temp")
+    },
+    filename: function (req, file, cb) {
+        const uniqueFilename = uuidv4() + file.originalname;
+        cb(null, uniqueFilename);
+    }
+})
+const upload = multer({
+    storage,
+})
+module.exports = upload 
