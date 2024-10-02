d6d381324717735df9db6979348a8dce8af1b401
Krish Jotaniya <krishjotaniya71@gmail.com>
2024-10-02 18:48:30 +0530

    Seeder Added to add admin data

---
+++ backend/admin-seeder.js
@@ -0,0 +1,41 @@
+const adminDetails = require("./models/Admin/details.model.js");
+const adminCredential = require("./models/Admin/credential.model.js");
+const connectToMongo = require("./database/db.js");
+const mongoose = require("mongoose");
+
+const seedData = async () => {
+    try {
+        await connectToMongo();
+
+        await adminCredential.deleteMany({})
+        await adminDetails.deleteMany({})
+
+        await adminCredential.create({
+            loginid: 123456,
+            password: "admin123"
+        });
+
+        const adminDetail = {
+            employeeId: "123456",
+            firstName: "Sundar",
+            middleName: "R",
+            lastName: "Pichai",
+            email: "sundarpichar@gmail.com",
+            phoneNumber: "1234567890",
+            gender: "Male",
+            type: "Admin",
+            profile: "/media/Faculty_Profile_123456.jpg",
+        };
+
+        await adminDetails.create(adminDetail);
+
+        console.log("Seeding completed successfully!");
+    } catch (error) {
+        console.error("Error while seeding:", error);
+    } finally {
+        await mongoose.connection.close();
+        process.exit();
+    }
+};
+
+seedData();
